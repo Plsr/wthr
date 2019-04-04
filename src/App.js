006aa2062fadc7dd358c1d35dd86d1ad70c2accc
Christian Poplawski <christianpoplawski@gmail.com>
2019-04-04 22:08:17 +0200

    First steps in implementing OWM Api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: true,
+      weatherData: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=cologne,de&APPID=${
+        process.env.REACT_APP_OWM_API_KEY
+      }`
+    ).then(res => {
+      if (res.status !== 200) {
+        console.error('Request failed')
+        console.error(res)
+        return
+      }
+      res.json().then(function(data) {
+        console.log(data)
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {this.state.loading && <div>Loading</div>}
+          {!this.state.loading && (
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          )}
         </header>
       </div>
     )
